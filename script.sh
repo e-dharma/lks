4a83375b407904d4b18487b0bf5528c34b59b1dd
e-dharma <80888357+e-dharma@users.noreply.github.com>
2021-03-18 16:55:20 +0700

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,9 @@
+sudo apt install default-jdk -y
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
+sudo apt update -y
+sudo apt install elasticsearch -y
+
+
+java -version
+sudo systemctl enable elasticsearch.service --now
